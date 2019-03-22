0212557b866dbb6fd71a63d592e8c4a781be7517
Irfan Pertadima <pertadimairfan@gmail.com>
2019-03-22 16:16:42 +0700

    [irfan] add depedency

---
+++ app/src/main/java/id/co/moviedb/networking/NetworkAdapter.kt
@@ -0,0 +1,11 @@
+package id.co.moviedb.networking
+
+/**
+ * Created by pertadima on 22,March,2019
+ */
+
+
+
+class NetworkAdapter {
+
+}
---
+++ app/src/main/java/id/co/moviedb/networking/NetworkService.kt
@@ -0,0 +1,9 @@
+package id.co.moviedb.networking
+
+/**
+ * Created by pertadima on 22,March,2019
+ */
+
+interface NetworkService {
+
+}
